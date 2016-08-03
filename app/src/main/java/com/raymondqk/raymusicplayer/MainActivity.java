e4f5e7412e731b7b525ec379037fb92d2ee3b218
Raymond-Chan <raymondcqk@163.com>
2016-08-04 04:20:33 +0800

    播放器主界面布局完成 接下来 获取歌曲时间，当前播放进度，歌曲信息，单曲，循环，随即播放 歌曲列表 sdcard扫描

--- app/src/main/java/com/raymondqk/raymusicplayer/MainActivity.java
+++ app/src/main/java/com/raymondqk/raymusicplayer/MainActivity.java
@@ -1,23 +1,64 @@
 package com.raymondqk.raymusicplayer;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.raymondqk.raymusicplayer.customview.AvatarCircle;
+import com.raymondqk.raymusicplayer.customview.MusicService;
 
 /**
  * Created by 陈其康 raymondchan on 2016/8/3 0003.
  */
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,MusicService.SetAvatarCallBack{
 
 
-    AvatarCircle mAvatarCircle;
+    private AvatarCircle mAvatarCircle;
     private boolean isClickAvatar;
+    private ImageButton mIb_play_mode;
+    private ImageButton mIb_play;
+    private ImageButton mIb_preview;
+    private ImageButton mIb_next;
+
+    private MusicService mMusicService;
+
+
+//    MusicService.SetAvatarCallBack mSetAvatarCallBack = new MusicService.SetAvatarCallBack() {
+//        @Override
+//        public void setAvatar(int resId) {
+//            mAvatarCircle.setImageResource(resId);
+//        }
+//    };
+
+    private ServiceConnection mServiceConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MusicService.MusicServiceBinder binder = (MusicService.MusicServiceBinder) service;
+            mMusicService = binder.getServiceInstance();
+            if (mMusicService != null) {
+                Toast.makeText(MainActivity.this, "音乐服务绑定成功", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "音乐服务绑定失败", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mMusicService = null;
+
+        }
+    };
+    private Intent mMusicSeviceIntent;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -30,17 +71,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         getSupportActionBar().setSubtitle("播放界面");
         mAvatarCircle = (AvatarCircle) findViewById(R.id.avatar_main);
         mAvatarCircle.setOnClickListener(this);
+        mIb_play_mode = (ImageButton) findViewById(R.id.ib_play_mode);
+        mIb_play = (ImageButton) findViewById(R.id.ib_play);
+        mIb_preview = (ImageButton) findViewById(R.id.ib_preview);
+        mIb_next = (ImageButton) findViewById(R.id.ib_next);
+
+        mIb_next.setOnClickListener(this);
+        mIb_play.setOnClickListener(this);
+        mIb_play_mode.setOnClickListener(this);
+        mIb_preview.setOnClickListener(this);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        serviceInit();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMusicService = null;
+        unbindService(mServiceConnection);
+        stopService(mMusicSeviceIntent);
+        Log.i("Test","MainActivity onDestroy");
+    }
+
+    private void serviceInit() {
+        mMusicSeviceIntent = new Intent();
+        mMusicSeviceIntent.setClass(MainActivity.this, MusicService.class);
+        bindService(mMusicSeviceIntent, mServiceConnection, BIND_AUTO_CREATE);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu,menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.menu_item_list:
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, MusicListActivity.class);
@@ -52,15 +130,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-switch (v.getId()){
-    case R.id.avatar_main:
-        if (isClickAvatar) {
-            mAvatarCircle.setImageResource(R.drawable.avatar_joyce2);
-            isClickAvatar = !isClickAvatar;
-        }else {
-            mAvatarCircle.setImageResource(R.drawable.avatar_joyce);
-            isClickAvatar = !isClickAvatar;
+        switch (v.getId()) {
+            case R.id.avatar_main:
+                if (isClickAvatar) {
+                    mAvatarCircle.setImageResource(R.drawable.avatar_joyce2);
+                    isClickAvatar = !isClickAvatar;
+                } else {
+                    mAvatarCircle.setImageResource(R.drawable.avatar_joyce);
+                    isClickAvatar = !isClickAvatar;
+                }
+                break;
+            case R.id.ib_play_mode:
+                if (mMusicService != null) {
+                    if (mMusicService.getPlay_mode() == MusicService.MODE_LOOP_ALL) {
+                        mMusicService.setPlay_mode(MusicService.MODE_LOOP_ONE);
+                        mIb_play_mode.setImageResource(R.drawable.loop_one);
+                        // TODO: 2016/8/4 0004 设置播放模式
+
+                    } else if (mMusicService.getPlay_mode() == MusicService.MODE_LOOP_ONE) {
+                        mMusicService.setPlay_mode(MusicService.MODE_RADOM);
+                        mIb_play_mode.setImageResource(R.drawable.radom);
+                        // TODO: 2016/8/4 0004 设置播放模式
+                    } else if (mMusicService.getPlay_mode() == MusicService.MODE_RADOM) {
+                        mMusicService.setPlay_mode(MusicService.MODE_LOOP_ALL);
+                        mIb_play_mode.setImageResource(R.drawable.loop_all);
+                    }
+                }
+                break;
+            case R.id.ib_play:
+                if (mMusicService != null) {
+                    if (mMusicService.getPlay_state() == MusicService.STATE_STOP) {
+                        mIb_play.setImageResource(R.drawable.pause);
+                        mMusicService.setPlay_state(MusicService.STATE_PLAYING);
+                        // TODO: 2016/8/4 0004 在Service里面进行音乐播放的操作
+                    } else {
+                        mIb_play.setImageResource(R.drawable.play);
+                        mMusicService.setPlay_state(MusicService.STATE_STOP);
+                        // TODO: 2016/8/4 0004 在Service里面进行音乐暂停的操作
+                    }
+                }
+                break;
+            case R.id.ib_next:
+                mMusicService.nextMusic();
+                mAvatarCircle.setImageResource(mMusicService.getCurrent_Avatar());
+                break;
+            case R.id.ib_preview:
+                mMusicService.previewMusic();
+                mAvatarCircle.setImageResource(mMusicService.getCurrent_Avatar());
+                break;
         }
-}
+    }
+
+    @Override
+    public void setAvatar(int resId) {
+        mAvatarCircle.setImageResource(resId);
     }
 }
---
+++ app/src/main/java/com/raymondqk/raymusicplayer/customview/MusicService.java
@@ -0,0 +1,193 @@
+package com.raymondqk.raymusicplayer.customview;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.raymondqk.raymusicplayer.R;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by 陈其康 raymondchan on 2016/8/4 0004.
+ */
+public class MusicService extends Service {
+
+    public static final int MODE_LOOP_ALL = 0;
+    public static final int MODE_LOOP_ONE = 1;
+    public static final int MODE_RADOM = 2;
+
+    public static final int STATE_PLAYING = 0;
+    public static final int STATE_PAUSE = 1;
+    public static final int STATE_STOP = 2;
+
+    public static final int MSG_STOP = 0;
+    public static final int MSG_PLAY = 1;
+    public static final int MSG_PAUSE = 2;
+    public static final int MSG_NEXT = 3;
+    public static final int MSG_PREVIEW = 4;
+
+
+    private int play_mode = MODE_LOOP_ALL;
+    private int play_state = STATE_STOP;
+
+    private boolean fisrtPlay = true;
+
+    public int getCurrent_Avatar() {
+        return current_Avatar;
+    }
+
+    private int current_Avatar = 0;
+
+
+
+    private int current_duration;
+
+
+    MusicServiceBinder mBinder = new MusicServiceBinder();
+    private MediaPlayer mMediaPlayer;
+
+    private ArrayList<Integer> mAvatarResIdList = new ArrayList<Integer>();
+    private int[] mAvatars = {R.drawable.avatar_joyce, R.drawable.avatar_bigbang};
+    private ArrayList<Uri> mMusicUriList = new ArrayList<Uri>();
+    private int currentIndex;
+
+    public interface SetAvatarCallBack {
+        void setAvatar(int resId);
+    }
+
+    //    SetAvatarCallBack mSetAvatarCallBack = new SetAvatarCallBack();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //记得释放资源
+        mMediaPlayer.stop();
+        mMediaPlayer.release();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_NOT_STICKY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //android 获取raw 绝对路径 -- raw资源转uri
+        Uri uri = Uri.parse("android.resource://com.raymondqk.raymusicplayer/" + R.raw.missyou);
+        mMusicUriList.add(uri);
+        mAvatarResIdList.add(R.drawable.avatar_joyce);
+
+        uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.stillalive);
+        mMusicUriList.add(uri);
+        mAvatarResIdList.add(R.drawable.avatar_bigbang);
+
+        mMediaPlayer = new MediaPlayer();
+    }
+
+
+    public void setPlay_mode(int play_mode) {
+        this.play_mode = play_mode;
+    }
+
+    public void setPlay_state(int play_state) {
+        this.play_state = play_state;
+        if (this.play_state == STATE_PLAYING) {
+            if (fisrtPlay){
+                playMusic();
+                fisrtPlay = false;
+            }else {
+                continueMusic();
+            }
+
+        } else if (this.play_state == STATE_STOP) {
+            pauseMusic();
+        }
+    }
+
+    private void continueMusic() {
+        mMediaPlayer.start();
+    }
+
+    private void pauseMusic() {
+        mMediaPlayer.pause();
+
+    }
+
+    public int getPlay_mode() {
+
+        return play_mode;
+    }
+
+    public int getPlay_state() {
+        return play_state;
+    }
+
+    public void nextMusic() {
+        // TODO: 2016/8/4 0004 播放下一首
+//        Toast.makeText(MusicService.this, "下一首", Toast.LENGTH_SHORT).show();
+        //        if (currentIndex > 0) {
+        //            currentIndex++;
+        //        }
+        currentIndex++;
+        current_Avatar = mAvatarResIdList.get(currentIndex % mAvatarResIdList.size());
+        mMediaPlayer.reset();
+        playMusic();
+
+
+    }
+
+    public void playMusic() {
+        try {
+            mMediaPlayer.setDataSource(this, mMusicUriList.get(currentIndex % mMusicUriList.size()));
+            mMediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Test", "无法播放音乐");
+            mMediaPlayer.reset();
+        }
+        current_duration =  mMediaPlayer.getDuration();
+        mMediaPlayer.start();
+
+    }
+
+    public void previewMusic() {
+        // TODO: 2016/8/4 0004
+//        Toast.makeText(MusicService.this, "上一首", Toast.LENGTH_SHORT).show();
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            //实现列表前一首到头时，直接跳到队尾。
+            currentIndex = mMusicUriList.size() - 1;
+        }
+        //        mSetAvatarCallBack.setAvatar(mAvatars[currentIndex % mAvatars.length]);
+        current_Avatar = mAvatarResIdList.get(currentIndex % mAvatarResIdList.size());
+        mMediaPlayer.reset();
+        playMusic();
+    }
+
+    public int getCurrent_duration() {
+        return current_duration;
+    }
+    public class MusicServiceBinder extends Binder {
+        public MusicService getServiceInstance() {
+            return MusicService.this;
+        }
+    }
+
+
+}
+
